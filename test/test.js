c8e36680806b9ef3b705db3e7ac24b8e21eb8609
Vitor Leal <vitorleal1@gmail.com>
2013-02-21 21:07:51 +0100

    aded more test and some comments in the code

--- test/test.js
+++ test/test.js
@@ -2,19 +2,36 @@ var fs       = require('fs');
 var should   = require('should');
 var findCine = require('../lib/find-cine');
 
-describe("Should bring the objects", function() {
-    it("near cinemas", function(done) {
-        findCine.near('Calle del pinar 6', function(err, results) {
-          results.should.have.property('total');
-          results.should.have.property('theaters');
-          done();
-        });
+describe("NEAR function", function() {
+  it("must have the total and theaters", function(done) {
+    findCine.near('Calle del pinar 6', function(err, results) {
+      results.should.have.property('total');
+      results.should.have.property('theaters');
+      done();
     });
+  });
 
-    it("total should be > 0", function(done) {
-        findCine.near('Calle del pinar 6', function(err, results) {
-          results.total.should.be.above(0);
-          done();
-        });
+  it("total should be > 0", function(done) {
+    findCine.near('Calle del pinar 6', function(err, results) {
+      results.total.should.be.above(0);
+      done();
     });
+  });
+});
+
+describe("THEATER id function", function() {
+  it("must have total and theaters", function(done) {
+    findCine.theaterId('Calle del pinar 6', 'f5620c58eb9a048e', function(err, results) {
+      results.should.have.property('total');
+      results.should.have.property('theaters');
+      done();
+    });
+  });
+
+  it("total should be = 1", function(done) {
+    findCine.theaterId('Calle del pinar 6', 'f5620c58eb9a048e', function(err, results) {
+      results.total.should.be.equal(1);
+      done();
+    });
+  });
 });
